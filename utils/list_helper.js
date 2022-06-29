54f5c42bf4c470be526d619b92391ffc5cc44d1e
Eric Chin <ericychin94@gmail.com>
2022-06-29 03:45:42 -0700

    Created totalLikes function and exported it

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,16 @@
+
+const totalLikes = (blogs) => {
+    const sum = blogs.reduce((accumulator, value) => {
+        return accumulator + value.likes;
+    }, 0);
+};
+
+
 const dummy = (blogs) => {
     return 1;
 };
 
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 };
