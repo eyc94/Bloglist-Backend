620633dd7d7a045ee39873158c77c065d5b90ea9
Eric Chin <ericychin94@gmail.com>
2022-07-01 03:40:56 -0700

    Added test helper function file and added beforeEach to initialize db

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
+const helper = require("./test_helper");
 const app = require("../app");
-
 const api = supertest(app);
+const Blog = require("../models/blog");
+
+beforeEach(async () => {
+    await Blog.deleteMany({});
+    const blogObjects = helper.initialBlogs
+        .map(blog => new Blog(blog));
+    const promiseArray = blogObjects.map(blog => blog.save());
+    await Promise.all(promiseArray);
+});
 
 test("Notes are returned as JSON", async () => {
     await api
@@ -13,7 +22,7 @@ test("Notes are returned as JSON", async () => {
 
 test("There is no note", async () => {
     const response = await api.get("/api/blogs");
-    expect(response.body).toHaveLength(0);
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
 });
 
 afterAll(() => {
